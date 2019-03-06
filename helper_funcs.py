f8422577e1e54e1acd24c0d1860ccc4ca4dd5d09
rbarner14 <emailryanbarner@gmail.com>
2019-03-05 16:58:43 -0800

    Creating performer and producer vectors for machine learning set up

--- helper_funcs.py
+++ helper_funcs.py
@@ -14,7 +14,43 @@ def quantify_performer_similarity(p1_id, p2_id):
 
     return similarity_score
 
-    
+
+def generate_producer_vectors():
+
+    # create list of producers
+    producers = Producer.query.order_by("producer_id")
+    producer_ids = []
+
+    for producer in producers:
+        producer_ids.append(producer.producer_id)
+
+    return producer_ids
+
+
+def generate_performer_vectors():
+
+    # create list of producers
+    performers = Performer.query.order_by("performer_id")
+    performer_ids = []
+
+    for performer in performers:
+        performer_ids.append(performer.performer_id)
+
+    return performer_ids
+
+
+def generate_performer_vectors():
+
+    # create list of producers
+    performers = Performer.query.order_by("performer_id")
+    performer_ids = []
+
+    for performer in performers:
+        performer_ids.append(performer.performer_id)
+
+    return performer_ids
+
+
 if __name__ == "__main__":
     # Added for module interactive convenience to work directly with database.
     

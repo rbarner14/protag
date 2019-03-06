92497d32b47d5996e4a4726ee10be17add62eac5
rbarner14 <emailryanbarner@gmail.com>
2019-03-06 15:39:01 -0800

    found nearest neighbors

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.pivot.html
 import pandas as pd 
 
-with open('seed_data/scores.csv') as f:
-    data = f.read()
+# with open('seed_data/scores.csv') as f:
+#     data = f.read()
 
 data = pd.read_csv('seed_data/scores.csv')
 

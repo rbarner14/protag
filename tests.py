92a49f350d2c07b2aadf0a599428dc262071fb7b
rbarner14 <emailryanbarner@gmail.com>
2019-03-08 00:19:59 -0800

    wrote and successfully tested 4 Flask tests

---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+
+from server import app
+
+class ProTagTests(unittest.TestCase):
+    """Tests for ProTag."""
+
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn(b"ProTag", result.data)
+
+    def test_network(self):
+        result = self.client.get("/network")
+        self.assertIn(b"Thanks", result.data)
+
+    def test_network_name(self):
+        result = self.client.get("/network")
+        self.assertNotIn(b"Ryan Barner, Full-Stack Engineer", result.data)
+
+    def test_resume(self):
+        result = self.client.get("/resume")
+        self.assertIn(b"Ryan Barner, Full-Stack Engineer", result.data)
+
+    # def test_search(self):
+    #     result = self.client.get("/search_result",
+    #                               data={'search_str': "Wonda"})
+    #     self.assertIn(b"WondaGurl", result.data)
+    #     self.assertNotIn(b"Pharrell", result.data)
+
+
+
+if __name__ == "__main__":
+    unittest.main()

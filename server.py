76c5f6d33bbb94b6e5095dfab58ee1126692f727
Ryan Barner <emailryanbarner@gmail.com>
2019-03-20 15:19:14 -0700

    Turning Flask debugger off for demo

--- server.py
+++ server.py
@@ -736,7 +736,7 @@ def resume():
 if __name__ == "__main__":
     # debug=True as it has to be True at when DebugToolbarExtension is invoked.
     
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
